4e0ceb7c5d9f9550efc3e6680f231f4f3cb3dce0
Лада Мартемьянова <lada/martt@gmail.com>
2024-04-15 21:44:59 +0500

    4.15. Шаблонизируй то

--- src/presenter/boardPresenter.js
+++ src/presenter/boardPresenter.js
@@ -6,14 +6,20 @@ import OpenFormBtnView from '../view/open-form-button-view.js';
 import { RenderPosition, render, replace } from '../framework/render.js';
 import CloseFormBtnView from '../view/close-form-button-view.js';
 import SaveFormBtnView from '../view/save-form-btn-view.js';
+import NoPointsView from '../view/no-points-view.js';
+import FilterFormView from '../view/filter-form-view.js';
+import MainTripView from '../view/main-trip-view.js';
+import { generateFilter } from '../mock/filters.js';
+import FilterItemView from '../view/filter-item-view.js';
 
 export default class BoardPresenter {
-
   #sortFormView = new SortFormView();
   #pointsListView = new PointsListView();
+  #noPointsView = new NoPointsView();
+  #mainTrip = new MainTripView();
+  #filterFormView = new FilterFormView();
   #container = null;
   #pointModel = null;
-  #formComponent = null;
   #boardPoint = [];
 
   constructor ({container, pointModel}) {
@@ -22,13 +28,25 @@ export default class BoardPresenter {
   }
 
   init() {
-    this.#boardPoint = [...this.pointModel.getPoints()];
+    this.#boardPoint = [...this.#pointModel.getPoints()];
+    const tripControls = document.querySelector('.trip-main__trip-controls');
+    const filterFormContainer = document.querySelector('.trip-controls__filters');
+    const filters = generateFilter(this.#boardPoint);
+    render(this.#filterFormView, filterFormContainer);
+    for (let i = 0; i < filters.length; i++) {
+      render(new FilterItemView(filters[i]), this.#filterFormView.element);
+    }
 
-    render(this.#sortFormView, this.container);
-    render(this.#pointsListView, this.container);
-
-    for (let i = 0; i < this.#boardPoint.length; i++) {
-      this.#renderPoint(this.#boardPoint[i]);
+    if (this.#boardPoint.length > 0) {
+      render(this.#mainTrip, tripControls, RenderPosition.BEFOREBEGIN);
+      render(this.#sortFormView, this.#container);
+      render(this.#pointsListView, this.#container);
+      for (let i = 0; i < this.#boardPoint.length; i++) {
+        this.#renderPoint(this.#boardPoint[i]);
+      }
+    }
+    else {
+      render(this.#noPointsView, this.#container);
     }
   }
 
